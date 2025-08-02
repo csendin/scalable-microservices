96d5a6de9a3db2d4cb0dfde6b9ccf0e547bccd42
csendin <caiosendindev@gmail.com>
2025-08-02 10:26:22 -0300

    feat(invoices): consume messages

--- app-invoices/src/http/server.ts
+++ app-invoices/src/http/server.ts
@@ -1,3 +1,4 @@
+import '../broker/subscriber.ts'
 
 import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
@@ -18,6 +19,6 @@ app.get('/health', () => {
     return 'OK'
 })
 
-app.listen({ host: '0.0.0.0', port: 3333 }).then(() => {
+app.listen({ host: '0.0.0.0', port: 3334 }).then(() => {
     console.log('[Invoices] HTTP server running!')
 })
